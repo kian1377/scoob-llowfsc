cc1065271621752bc4f1ba1031f0693d1c7b0352
Kian Milani <kianmilani@email.arizona.edu>
2025-02-20 11:19:02 -0700

    tested LLOWFSC with 2.75mm of defocus

--- scoob_llowfsc/detector.py
+++ scoob_llowfsc/detector.py
@@ -6,17 +6,20 @@ import copy
 
 class DETECTOR():
 
-    def __init__(self,
-                 exp_time=0.001, 
-                 gain=1, 
-                 read_noise=5, 
-                 blacklevel=5,
-                 dark_current=1, 
-                 qe=0.5, 
-                 Nbits=16,
-                 ):
+    def __init__(
+            self,
+            exp_time=0.001, 
+            throughput=0.1,
+            gain=1, 
+            read_noise=5, 
+            blacklevel=5,
+            dark_current=1, 
+            qe=0.5, 
+            Nbits=16,
+        ):
         self.exp_time = exp_time
         self.qe = qe
+        self.throughput = throughput
         self.read_noise = read_noise
         self.blacklevel = blacklevel
         self.dark_current = dark_current
@@ -25,7 +28,8 @@ class DETECTOR():
         self.gain = gain
 
     def add_noise(self, flux_image):
-        ph_counts = xp.random.poisson(flux_image * self.exp_time)
+        flux = self.throughput * flux_image
+        ph_counts = xp.random.poisson(flux * self.exp_time)
         e_counts = self.qe * ph_counts
         det_counts = self.gain * e_counts
         det_counts += xp.random.poisson(self.blacklevel*xp.ones_like(flux_image)) # add blacklevel with poisson noise applied
